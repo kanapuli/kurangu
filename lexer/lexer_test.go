6509fcd28fe7dfcd2627441f80877a0c575a3732
Athavan kanapuli <akan@hellofresh.com>
2021-02-07 18:24:07 +0100

    ADD lexer support to identify boolean keywords, if-else and return keywords

--- lexer/lexer_test.go
+++ lexer/lexer_test.go
@@ -89,3 +89,50 @@ func TestNextToken(t *testing.T) {
 		}
 	}
 }
+func TestNextTokenWithBools(t *testing.T) {
+	input := ` if (5 < 10) {
+	return true;
+	} else {
+	return false;
+	}
+
+	`
+
+	tests := []struct {
+		expectedType    token.TokenType
+		expectedLiteral string
+	}{
+		{token.IF, "if"},
+		{token.LPAREN, "("},
+		{token.INT, "5"},
+		{token.LT, "<"},
+		{token.INT, "10"},
+		{token.RPAREN, ")"},
+		{token.LBRACE, "{"},
+		{token.RETURN, "return"},
+		{token.TRUE, "true"},
+		{token.SEMICOLON, ";"},
+		{token.RBRACE, "}"},
+		{token.ELSE, "else"},
+		{token.LBRACE, "{"},
+		{token.RETURN, "return"},
+		{token.FALSE, "false"},
+		{token.SEMICOLON, ";"},
+		{token.RBRACE, "}"},
+	}
+
+	l := New(input)
+
+	for i, tt := range tests {
+		tok := l.NextToken()
+
+		if tok.Type != tt.expectedType {
+			t.Fatalf("bool tests [%d] - tokentype wrong. expected=%q, got=%q", i, tt.expectedType, tok.Type)
+		}
+
+		if tok.Literal != tt.expectedLiteral {
+			t.Fatalf("tests [%d] - literal wrong. expected=%q, got=%q", i, tt.expectedLiteral, tok.Literal)
+
+		}
+	}
+}
--- token/token.go
+++ token/token.go
@@ -40,11 +40,21 @@ const (
 	// Keywords
 	FUNCTION = "FUNCTION"
 	LET      = "LET"
+	IF       = "IF"
+	ELSE     = "ELSE"
+	TRUE     = "TRUE"
+	FALSE    = "FALSE"
+	RETURN   = "RETURN"
 )
 
 var keywords = map[string]TokenType{
-	"fn":  FUNCTION,
-	"let": LET,
+	"fn":     FUNCTION,
+	"let":    LET,
+	"if":     IF,
+	"else":   ELSE,
+	"true":   TRUE,
+	"false":  FALSE,
+	"return": RETURN,
 }
 
 func LookupIdent(ident string) TokenType {
